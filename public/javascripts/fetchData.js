5d26109a228a4cb1dfc0aad78945b8555c1b9bda
heahl <hendrik712.a@gmail.com>
2025-11-10 22:26:57 +0100

    add all post functions based on api-docs

--- public/javascripts/fetchData.js
+++ public/javascripts/fetchData.js
@@ -166,3 +166,110 @@ async function getEverybody(){
     }
 }
 
+// === POST ===
+
+/**
+ * Schreibt kaufen/verkaufen von Aktien in das depot des Nutzers
+ *
+ * @param stockName :string name der Aktie
+ * @param number :number Anzahl der zu kaufenden/verkaufenden Positionen
+ * @returns {Promise<any|null>} Objekt | null bei Fehler
+ */
+async function postPositions(stockName, number){
+    // Typ prüfen
+    if (typeof stockName !== "string"||typeof number !== "number"){
+        console.error('Ungültige Parameter');
+        return null;
+    }
+    // Wertebereich prüfen
+    if (number===0){
+        console.error('Anzahl darf nicht 0 sein');
+        return null;
+    }
+    // Stringlänge prüfen
+    if (stockName.length>50||stockName.length===0){
+        console.error('Ungültiger Aktienname');
+        return null;
+    }
+    try{
+        const response = await fetch('api/account/positions',{
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"},
+            body: JSON.stringify({
+                stock: {
+                    name: stockName
+                },
+                number: number
+            })
+        });
+
+        switch (response.status){
+            case 201:
+                return await response.json();
+            case 422:
+                console.error('Ungültige Eingabe/unzureichende Deckung:',await response.json());
+                return null;
+            case 500:
+               console.error('Internal Server Error:', await response.json());
+               return null;
+            default:
+                console.error('Fehler:'+ response.status);
+                return null;
+        }
+    }catch (e){
+        console.error('Netzwerkfehler:', e.message);
+        return null;
+    }
+}
+
+/**
+ * Sendet Nachrichten an andere Nutzer
+ *
+ * @param recipient :string Empfänger der Nachricht. Broadcast wird von der api nicht unterstützt...
+ * @param message :string zu sendende Nachricht
+ * @returns {Promise<any|null>} Objekt | null bei Fehler
+ */
+async function postMessages(recipient, message){
+    // Typ prüfen
+    if(typeof recipient!== string||typeof message!== string){
+        console.error('Ungültige Parameter: beide müssen string sein.');
+        return null;
+    }
+   // Länge prüfen
+   if(recipient.trim().length===0||message.trim().length ===0) {
+       console.error('Empfänger / Nachricht zu kurz');
+       return null;
+   }
+   if(recipient.length>50||message.length>500){
+       console.error('Empfänger / Nachricht zu lang');
+       return null;
+   }
+    try {
+       const response = await fetch('api/messages',{
+           method: "POST",
+           headers:{
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify({
+               recipient: recipient,
+               message: message
+           })
+       })
+
+        switch (response.status){
+            case 200:
+                return await response.json();
+            case 422:
+                console.error('Invalid input:', await response.json());
+                return null;
+            case 500:
+                console.error('Internal Server Error:', await response.json());
+                return null;
+            default:
+               console.error('Fehler:', response.status);
+        }
+    }catch (e){
+       console.error("Netzwerkfehler:", e.message);
+    }
+}
